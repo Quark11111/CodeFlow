bba965d95db901666d474e1004a1c06bed660042
Tami Lin <timasvx@gmail.com>
2026-06-14 02:47:34 +0500

    Finally

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
+  plugins: [
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          title: 'CodeFlow'
+        }
+      }
+    })
+  ],
   build: {
     rollupOptions: {
       input: {
-        main: './index.html',
-        admin: './admin/index.html'
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin/index.html'),
+        dashboard: resolve(__dirname, 'dashboard/index.html')
       }
     }
   }
